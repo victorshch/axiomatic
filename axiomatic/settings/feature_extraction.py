5cf4204e8295005bd7ee09804d8bae7e67f02ce1
teplyakova <natalia.teplyakova95@gmail.com>
2017-04-08 21:52:29 +0300

    Fix settings for feature extraction step

--- axiomatic/settings/feature_extraction.py
+++ axiomatic/settings/feature_extraction.py
@@ -1,5 +1,13 @@
-from ..features import Maximum, Minimum, Mean, LinearRegressionCoef, StdDeviation
+from ..features import (
+    Maximum,
+    Minimum,
+    Mean,
+    StdDeviation,
+    Kurtosis,
+    Skewness,
+    LinearRegressionCoef,
+)
 
 DEFAULT_SAMPLE_LENGTH = 20
 DEFAULT_RATIO = 0.3
-DEFAULT_FEATURES = [Maximum(), Minimum(), Mean(), LinearRegressionCoef(), StdDeviation()]
+DEFAULT_FEATURES = [Maximum(), Minimum(), Mean(), StdDeviation(), Kurtosis(), Skewness(), LinearRegressionCoef()]
