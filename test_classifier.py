45668d568b60a5a6a4e91688fd93faa7f6c936ab
teplyakova <natalia.teplyakova95@gmail.com>
2017-04-10 17:07:51 +0300

    Fix test for classifier

--- test_classifier.py
+++ test_classifier.py
@@ -1,37 +1,39 @@
 import pickle
+from sklearn.metrics import accuracy_score
 
 from axiomatic.base import TrainingPipeline
+from axiomatic.utils import transform_dataset
 from axiomatic.axiom_training_stage import KMeansClusteringAxiomStage
-from axiomatic.recognizer_training_stage import ClassifierRecognizerTrainingStage
-from axiomatic.objective_function import Accuracy
-from axiomatic.abnormal_behavior_recognizer import Classifier
+from axiomatic.classifier_training_stage import ClassifierTrainingStage
+from axiomatic.ts_classifier import TimeSeriesClassifier
 
 
 with open('datasets/debug_classifier_dataset.pickle', 'rb') as f:
     dataset = pickle.load(f)
 
-clustering_axiom_stage = KMeansClusteringAxiomStage({'clustering_params': {'n_clusters': 15,
-        'init': 'k-means++', 'n_init': 10}, 'feature_extraction_params': {'sample_length': 20, 'ratio': 0.2}})
+clustering_axiom_stage = KMeansClusteringAxiomStage(
+    {
+        'clustering_params': {'n_clusters': 15, 'init': 'k-means++', 'n_init': 10},
+        'feature_extraction_params': {'sample_length': 20, 'ratio': 0.2},
+    }
+)
 
 # stage for constructing axiom system and abnormal models
-# abn_models_type == 'brute_force' for exhaustive search of models with fixed length
-# abn_models_type == 'genetic' for constructing abnormal models using genetic algorithm
-classifier_recognizer_stage = ClassifierRecognizerTrainingStage(abn_models_type='brute_force')
+classifier_training_stage = ClassifierTrainingStage()
 
-training_pipeline = TrainingPipeline([clustering_axiom_stage, classifier_recognizer_stage])
+training_pipeline = TrainingPipeline([clustering_axiom_stage, classifier_training_stage])
 
 artifacts = training_pipeline.train(dataset)
 
 print "Artifacts after training: ", artifacts
 
-classifier = Classifier(artifacts['axiom_system'], artifacts['abn_models'])
+classifier = TimeSeriesClassifier(artifacts['axiom_system'], artifacts['abn_models'])
 
 # make list of time series and list of class labels
 ts_list, class_labels = transform_dataset(dataset['test'])
 predictions = classifier.predict(ts_list)
 
 # count accuracy
-obj_fn = Accuracy()
-obj_fn_value = obj_fn.calculate(class_labels, predictions)
+obj_fn_value = accuracy_score(class_labels, predictions)
 
 print "Accuracy: ", obj_fn_value
