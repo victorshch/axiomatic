d440221de70f4bb90349bda9a896fff15fafed43
alex_world <ols1959@gmail.com>
2017-05-17 00:02:01 +0300

    launcher and datasets

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,12 +1,15 @@
 from datasets.synthetic2.gen import generate_dataset
 from datasets.synthetic2.config import dataset_config
-import pickle
+import pickle, sys
 
-dataset_config['dimension_count'] = int(input('Number of dimensions (1 or 2): '))
-number_of_classes = int(input('Number of classes (1 or 2): '))
+dataset_config['dimension_count'] = int(sys.argv[1])
+number_of_classes = int(sys.argv[2])
 
 if number_of_classes == 1:
     dataset_config['classes'].pop('bad2')
 
+dataset_config['noise']['amp_distortion'] = float(sys.argv[3])
+dataset_config['noise']['time_distortion'] = float(sys.argv[4])
+
 dataset = generate_dataset(**dataset_config)
-pickle.dump(dataset, open('dataset_{0}_{1}.pickle'.format(dataset_config['dimension_count'], number_of_classes), 'wb'))
+pickle.dump(dataset, open('dataset.pickle', 'wb'))
--- launch.py
+++ launch.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import pickle
+import sys
 
 from axiomatic.base import AxiomSystem, TrainingPipeline
 from axiomatic.axiom_training_stage import *
@@ -9,21 +10,27 @@ from axiomatic.elementary_conditions import *
 from axiomatic.objective_function import ObjectiveFunction
 from axiomatic.abnormal_behavior_recognizer import AbnormalBehaviorRecognizer
 
-with open('dataset_1_1.pickle', 'rb') as f:
+with open('dataset.pickle', 'rb') as f:
     dataset = pickle.load(f)
 
 axiom_list = [MinMaxAxiom, MaxAxiom, MinAxiom, ChangeAxiom, IntegralAxiom, RelativeChangeAxiom, FirstDiffAxiom, SecondDiffAxiom]
-frequency_ec_stage = FrequencyECTrainingStage({'num_part': 50, 'left_window': 5, 'right_window': 5, 'num_axioms': 20, 'axiom_list': axiom_list, 'enable_cache': True})
-frequency_axiom_stage = FrequencyAxiomTrainingStage({'num_axioms': 20, 'max_depth': 10, 'num_step_axioms': 5})
+frequency_ec_stage = FrequencyECTrainingStage({'num_part': 100, 'left_window': 5, 'right_window': 5, 'num_axioms': 20, 'axiom_list': axiom_list, 'enable_cache': True})
+frequency_axiom_stage = FrequencyAxiomTrainingStage({'num_axioms': 10, 'max_depth': 5, 'num_step_axioms': 20})
 
-clustering_axiom_stage = KMeansClusteringAxiomStage({'clustering_params': {'n_clusters': 5,
+clustering_axiom_stage = KMeansClusteringAxiomStage({'clustering_params': {'n_clusters': 10,
         'init': 'k-means++', 'n_init': 10}, 'feature_extraction_params': {'sample_length': 20, 'ratio': 0.2}})
 
-genetic_recognizer_stage = GeneticRecognizerTrainingStage(dict(n_jobs=1, population_size=30, iteration_count=30, use_question_mark=False, num_axioms_weight=0.1))
+genetic_recognizer_stage = GeneticRecognizerTrainingStage(dict(n_jobs=1, population_size=30, iteration_count=20, use_question_mark=False, num_axioms_weight=0.1))
 
 frequency_stage = TrainingPipeline([frequency_ec_stage, frequency_axiom_stage])
 axiom_union_stage = AxiomUnionStage([frequency_stage, clustering_axiom_stage])
-training_pipeline = TrainingPipeline([axiom_union_stage, genetic_recognizer_stage])
+
+if sys.argv[1] == "cluster":
+  training_pipeline = TrainingPipeline([clustering_axiom_stage, genetic_recognizer_stage])
+elif sys.argv[1] == "freq":
+  training_pipeline = TrainingPipeline([frequency_stage, genetic_recognizer_stage])
+else:
+  training_pipeline = TrainingPipeline([axiom_union_stage, genetic_recognizer_stage])
 
 artifacts = training_pipeline.train(dataset, dict())
 
